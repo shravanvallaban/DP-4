8adf37d12ab47e66c7cdddeade0d3636368c9137
Shravan Vallaban <kuppu.shravan@gmail.com>
2024-06-01 20:46:44 -0700

    corrected the code order

--- Problem2.java
+++ Problem2.java
@@ -1,30 +1,25 @@
 package DP-4;
-// TC: O(m*n)
+
+// TC: O(k*n)
 // SC: O(n)
 public class Problem2 {
     
-    public int maximalSquare(char[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        if (matrix.length == 0 || matrix == null)
-            return 0;
-        int[] dp = new int[n + 1];
-        int maxValue = 0;
-
-        for (int i = 1; i <= m; i++) {
-            int diagVal = 0;
-            for (int j = 1; j <= n; j++) {
-                if (matrix[i - 1][j - 1] == '1') {
-                    int temp = dp[j];
-                    dp[j] = 1 + Math.min(dp[j], Math.min(dp[j - 1], diagVal));
-                    maxValue = Math.max(maxValue, dp[j]);
-                    diagVal = temp;
+   
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        int m = arr.length;
+        int[] dp = new int[m];
+        dp[0] = arr[0];
+        for (int i = 1; i < m; i++) {
+            int max = arr[i];
+            for (int j = 1; j <= k && i - j + 1 >= 0; j++) {
+                max = Math.max(arr[i - j + 1], max);
+                if (i - j >= 0) {
+                    dp[i] = Math.max(dp[i], max * j + dp[i - j]);
                 } else {
-                    dp[j] = 0;
+                    dp[i] = Math.max(dp[i], max * j);
                 }
-
             }
         }
-        return maxValue * maxValue;
+        return dp[m - 1];
     }
 }
